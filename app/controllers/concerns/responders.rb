1e35108dbd6b0fe02d9c07668994086dbb72fa90
calvin <calvodira@gmail.com>
2022-03-21 04:35:07 +0300

    refactor responder

--- app/controllers/concerns/responders.rb
+++ app/controllers/concerns/responders.rb
@@ -3,7 +3,7 @@ module Responders
 
   private
 
-  def error_response(message, status = :unprocessable_entity, context = {})
-    render status, json: { error: message }.merge(context)
+  def error_response(message, response_status = :unprocessable_entity, context = {})
+    render status: response_status, json: { error: message }.merge(context)
   end
 end
